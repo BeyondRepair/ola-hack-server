628383eaa627d08c22e9c5013bdbc5b209e461cb
Arvind <arvindkhadri@gmail.com>
2015-09-26 23:49:40 +0530

    Code refactor
    Make code modular, add variable to config.

--- olaCarPool/ola.py
+++ olaCarPool/ola.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response
 from flask.ext.pymongo import PyMongo
 from config import DefaultConfig
-
+import requests
 
 app = Flask(__name__)
 config = DefaultConfig()
@@ -11,8 +11,26 @@ mongo = PyMongo(app)
 
 @app.route('/book', methods=['POST'])
 def book_ride():
-    mongo.db.rides.save(request.get_json()['ride'])
+    try:
+        payload = {'pickup_lat': request.get_json()['ride']['pickup_lat'],
+                   'pickup_lng': request.get_json()['ride']['pickup_lon'],
+                   'pickup_mode': 'NOW',
+                   'category': request.get_json()['ride']['category']}
+    except KeyError:
+        serverResponse = make_response()
+        serverResponse.status_code = 404
+        return serverResponse
+    response = requests.get('http://sandbox-t.olacabs.com/v1/bookings/create?',
+                            payload,
+                            headers={'Authorization':
+                                     'Bearer %s' % request.get_json()[
+                                         'ride']['access_token'],
+                                     'X-APP-TOKEN':
+                                     app.config.get('X_APP_TOKEN')})
+    if response.status_code == 200:
+        mongo.db.rides.save(request.get_json()['ride'])
     return make_response()
+
 
 if __name__ == "__main__":
     app.run()
--- olaCarPool/sampleConfig.py
+++ olaCarPool/sampleConfig.py
@@ -3,3 +3,4 @@ class DefaultConfig():
     HOST = '127.0.0.1'
     PORT = 5000
     MONGO_DBNAME = 'ola_hack'
+    X_APP_TOKEN = 'your app token'
